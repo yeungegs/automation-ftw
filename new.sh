11b07631874d8185a70bd2c55dcb1496889bf041
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-11 21:28:39 -0700

    :recycle: Update script to make new directory in repo

--- new.sh
+++ new.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+new_dir=0
 source=0
 tasks=0
 header=0
-
+echo -n "Enter name of new directory > "
+read new_dir
 echo -n "Enter name of source file > "
 read source
 echo -n "Enter number of mandatory tasks > "
@@ -10,17 +12,26 @@ read tasks
 echo -n "is header needed? y/n > "
 read header
 
-grep "<li>File: <code>" $source | cut -c 19- | rev | cut -c 13- | rev | paste -s | xargs touch
-grep "<li>File: <code>" $source | cut -c 19- | rev | cut -c 13- | rev >> README.md
+mkdir $new_dir
+cd $new_dir
+files=../$new_dir/*
+
+grep "<li>File: <code>" ../$source | cut -c 19- | rev | cut -c 13- | rev | paste -s | xargs touch
+for f in $files
+do
+    echo "#\!/usr/bin/python3" >> $f
+done
+
+grep "<li>File: <code>" ../$source | cut -c 19- | rev | cut -c 13- | rev >> README.md
 
 if [ $header = y ]
 then
-    grep "<li>Prototype: <code>" $source | cut -c 24- | rev | cut -c 13- | rev >> header.h
+    grep "<li>Prototype: <code>" ../$source | cut -c 24- | rev | cut -c 13- | rev >> header.h
 else
     echo "no header file for you!"
 fi    
 
 for NUM in `seq 0 1 $tasks`
 do
-    $NUM-main.py
+    touch $NUM-main.py
 done
