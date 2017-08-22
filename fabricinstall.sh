5147091162e0adf94165232ff03040984cb3fb65
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-08-21 17:53:10 -0700

    Add script that installs pip3 and fabric

---
+++ fabricinstall.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+sudo apt-get install -y python3-pip
+sudo pip3 uninstall Fabric
+sudo apt-get install -y libffi-dev
+sudo apt-get install -y libssl-dev
+sudo apt-get install -y python3.4-dev
+sudo apt-get install -y libpython3-dev
+sudo pip3 install pyparsing
+sudo pip3 install appdirs
+sudo pip3 install Fabric3
