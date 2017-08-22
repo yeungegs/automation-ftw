80405013d325947ef3ccc1832c10d2fc19f3fff7
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-08-22 13:41:09 -0700

    Update new project script to specify what shebang is needed and whether new files should be executable

--- new_project.sh
+++ new_project.sh
@@ -14,20 +14,41 @@ echo -n "Enter file extension for main files (don't include '.') > "
 read ext
 echo -n "is header needed? y/n > "
 read header
+echo -n "are main files needed? y/n > "
+read main
+echo -n "should files be executable? y/n > "
+read permission
+echo -n "do you need a shebang? bash/py > "
+read shebang
 
+#make directory for new project
 mkdir $new_dir
 cd $new_dir
 files=../$new_dir/*
 
+#create empty files for project
 grep "<li>File: <code>" ../$source | cut -c 28- | rev | cut -c 13- | rev | paste -s | xargs touch
 
-for f in $files
-do
-    echo "#!/usr/bin/" >> $f
+#echo shebang into first line of new files
+for f in $files; do
+    if [ $permission = y ]
+       then chmod u+x $f
+    fi
+
+    if [ $shebang = bash ]
+       then echo '#!/bin/bash' >> $f
+    fi
+
+    if [ $shebang = py ]
+       then echo '#!/usr/bin/python' >> $f
+    fi
 done
 
+# prep README with list of files and description of tasks
 grep "<li>File: <code>" ../$source | cut -c 28- | rev | cut -c 13- | rev >> README.md
 sed -n -e '/Task Body/,/pre/ p' ../$source | grep -v "pre" >> README.md
+
+# prep header file for c projects
 if [ $header = y ]
 then
     grep "<li>Prototype: <code>" ../$source | cut -c 24- | rev | cut -c 13- | rev >> header.h
@@ -35,8 +56,12 @@ else
     echo "no header file for you!"
 fi    
 
-for NUM in `seq 0 1 $tasks`
-do
-    touch $NUM-main.$ext
-done
+# prep main files if requested
+if [ $main = y ]
+then
+    for NUM in `seq 0 1 $tasks`
+    do
+	touch $NUM-main.$ext
+    done
+fi
 echo -n "gl hf~!"
