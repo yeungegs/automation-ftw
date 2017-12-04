9afb82b4e09f46f5876142f4cbb6e43d19e6ef9c
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-12-04 14:27:47 -0800

    Update fabric script to match changes from @papamuziko

--- fabricinstall.sh
+++ fabricinstall.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
-sudo apt-get install -y python3-pip
+# bash script to automate the installation of Fabric for Python3
 sudo pip3 uninstall Fabric
 sudo apt-get install -y libffi-dev
 sudo apt-get install -y libssl-dev
+sudo apt-get install -y build-essential
 sudo apt-get install -y python3.4-dev
 sudo apt-get install -y libpython3-dev
 sudo pip3 install pyparsing
 sudo pip3 install appdirs
+sudo pip3 install --upgrade setuptools
+sudo pip3 install cryptography
 sudo pip3 install Fabric3
